5007f6aeb39859126ff39c00099ce046315c5c9b
romanmunar <roman.munar1@gmail.com>
2021-04-14 18:19:17 +0800

    installed line clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      lineClamp: {
+        10: '10',
+        12: '12',
+        16: '16',
+        20: '20',
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
