d5c5e4a9da2b4f7e152bfb70ec6f7478dda874e2
romanmunar <roman.munar1@gmail.com>
2021-04-15 11:08:36 +0800

    added controllers

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -8,71 +8,94 @@ interface LocalStorage {
   watchedEpisodes: Episode[]
 }
 
-const getLocalStorageItem = (item: keyof LocalStorage) => {
+export type FilterId = keyof LocalStorage
+
+interface Controller<T> {
+  id: FilterId
+  get: () => T[]
+  set: (items: T[]) => T[]
+  add: (item: T) => T[]
+  remove: (item: T) => T[]
+}
+
+export const StarController: Controller<Anime> = {
+  id: 'starredAnimes',
+  get: getStarredAnimes,
+  set: setStarredAnimes,
+  add: addStarAnime,
+  remove: removeStarAnime,
+}
+
+export const HeartController: Controller<Anime> = {
+  id: 'heartedAnimes',
+  get: getHeartedAnimes,
+  set: setHeartedAnimes,
+  add: addHeartAnime,
+  remove: removeHeartAnime,
+}
+
+const getLocalStorageItem = (item: FilterId) => {
   const items = JSON.parse(localStorage.getItem(item)!)
   return items ? items : setLocalStorageItem(item, [])
 }
 
-const setLocalStorageItem = (
-  item: keyof LocalStorage,
-  items: (Anime | Episode)[]
-) => {
+const setLocalStorageItem = (item: FilterId, items: (Anime | Episode)[]) => {
   localStorage.setItem(item, JSON.stringify(items))
   return items
 }
 
-export const getStarredAnimes = () => {
+export function getStarredAnimes() {
   return getLocalStorageItem('starredAnimes') as Anime[]
 }
 
-export const setStarredAnimes = (animes: Anime[]) => {
+export function setStarredAnimes(animes: Anime[]) {
   const uniqueAnimes = filterByUniqueField(animes, 'id')
   return setLocalStorageItem('starredAnimes', uniqueAnimes) as Anime[]
 }
 
-export const addStarAnime = (anime: Anime) => {
+export function addStarAnime(anime: Anime) {
   const starredAnimes = [...getStarredAnimes(), anime]
   return setStarredAnimes(starredAnimes)
 }
 
-export const removeStarAnime = (anime: Anime) => {
+export function removeStarAnime(anime: Anime) {
   const starredAnimes = filterByUniqueField(getStarredAnimes(), 'id')
   return setStarredAnimes(starredAnimes)
 }
 
-export const getHeartedAnimes = () => {
+export function getHeartedAnimes() {
   return getLocalStorageItem('heartedAnimes') as Anime[]
 }
 
-export const setHeartedAnimes = (animes: Anime[]) => {
+export function setHeartedAnimes(animes: Anime[]) {
   const uniqueAnimes = filterByUniqueField(animes, 'id')
   return setLocalStorageItem('heartedAnimes', uniqueAnimes) as Anime[]
 }
 
-export const addHeartAnime = (anime: Anime) => {
+export function addHeartAnime(anime: Anime) {
   const heartedAnimes = [...getHeartedAnimes(), anime]
   return setHeartedAnimes(heartedAnimes)
 }
 
-export const removeHeartAnime = (anime: Anime) => {
+export function removeHeartAnime(anime: Anime) {
   const heartedAnimes = filterByUniqueField(getHeartedAnimes(), 'id')
   return setHeartedAnimes(heartedAnimes)
 }
 
-export const getWatchedEpisodes = () => {
+export function getWatchedEpisodes() {
   return getLocalStorageItem('watchedEpisodes') as Episode[]
 }
 
-export const setWatchedEpisodes = (episodes: Episode[]) => {
+export function setWatchedEpisodes(episodes: Episode[]) {
   return setLocalStorageItem('watchedEpisodes', episodes) as Episode[]
 }
 
-export const addWatchedEpisode = (episode: Episode) => {
+export function addWatchedEpisode(episode: Episode) {
   const watchedEpisodes = [...getWatchedEpisodes(), episode]
   return setWatchedEpisodes(watchedEpisodes)
 }
 
-export const removeWatchedEpisode = (episode: Episode) => {
+export function removeWatchedEpisode(episode: Episode) {
   const watchedEpisodes = filterByUniqueField(getWatchedEpisodes(), 'id')
   return setWatchedEpisodes(watchedEpisodes)
 }
