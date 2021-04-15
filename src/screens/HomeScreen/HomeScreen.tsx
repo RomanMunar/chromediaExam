d35553687275b87b455b4960897b8fde18af4bbf
romanmunar <roman.munar1@gmail.com>
2021-04-15 11:12:30 +0800

    finish pagination on all filters

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,57 +1,91 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Pagination } from '../../components'
-import { offset } from '../../constants'
 import { AnimeCard } from '../../components/anime'
 import { Reset } from '../../components/icons'
 import Layout from '../../components/layout'
+import { useFilter } from '../../hooks/useFilter'
 import { useSearch } from '../../hooks/useSearch'
-import { getHeartedAnimes, getStarredAnimes } from '../../localStorage'
+import { Anime } from '../../models/anime'
 import Filter from './Filter'
 import SearchBar from './SearchBar'
 
 export type FilterType = 'star' | 'heart' | 'none'
 
+export interface PageConfig {
+  totalCount: number
+  offset?: number
+  pages?: number
+  page: number
+  resultsCount: number
+  setPage: (page: number) => void
+}
 const HomeScreen = () => {
   const [filter, setFilter] = useState<FilterType>('none')
-  const {
-    searchKeyword,
+  const [pageConfig, setPageConfig] = useState<PageConfig>()
+  const [displayedAnimes, setDisplayedAnimes] = useState<Anime[]>()
+  const [
+    starAnimes,
+    starPage,
+    setStarPage,
+    starTotalCount,
+    starAnimesCount,
+  ] = useFilter(filter, 'starredAnimes')
+  const [
+    heartAnimes,
+    heartPage,
+    setHeartPage,
+    heartTotalCount,
+    heartAnimesCount,
+  ] = useFilter(filter, 'heartedAnimes')
+  const [
     loading,
     animes,
-    resultsCount,
-    setResultsCount,
-    setAnimes,
-    setPage,
     page,
+    setPage,
+    searchKeyword,
     totalCount,
-  } = useSearch(filter)
+    resultsCount,
+  ] = useSearch(filter)
 
-  const onStarFilterClick = () => {
-    if (filter === 'star') return setFilter('none') //toggle filter
-    const animes = getStarredAnimes()
-    setFilter('star')
-    setAnimes(animes)
-    setResultsCount(animes.length)
-  }
+  useEffect(() => {
+    setPageConfig({
+      page: starPage,
+      setPage: setStarPage,
+      totalCount: starTotalCount,
+      resultsCount: starAnimesCount,
+    })
+    setDisplayedAnimes(starAnimes)
+    //eslint-disable-next-line
+  }, [starPage, starAnimes])
 
-  const onHeartFilterClick = () => {
-    if (filter === 'heart') return setFilter('none') //toggle filter
-    const animes = getHeartedAnimes()
-    setFilter('heart')
-    setAnimes(animes)
-    setResultsCount(animes.length)
-  }
+  useEffect(() => {
+    setPageConfig({
+      page: heartPage,
+      setPage: setHeartPage,
+      totalCount: heartTotalCount,
+      resultsCount: heartAnimesCount,
+    })
+    setDisplayedAnimes(heartAnimes)
+    //eslint-disable-next-line
+  }, [heartPage, heartAnimes])
+
+  useEffect(() => {
+    setPageConfig({
+      setPage,
+      page,
+      totalCount,
+      resultsCount,
+    })
+    setDisplayedAnimes(animes)
+    //eslint-disable-next-line
+  }, [page, animes])
 
   return (
     <Layout>
       <div className="sticky top-0 z-50 flex-none bg-blue-50">
         <div className="max-w-6xl mx-auto">
           <div className="flex items-center justify-between p-4 space-x-6">
-            <Filter
-              onResetFilterClick={() => setFilter('none')}
-              onStarFilterClick={onStarFilterClick}
-              onHeartFilterClick={onHeartFilterClick}
-              filter={filter}
-            />
+            <Filter setFilter={setFilter} filter={filter} />
             <div className="flex-grow hidden w-full max-w-lg mx-auto md:block">
               <SearchBar />
             </div>
@@ -67,23 +101,19 @@ const HomeScreen = () => {
       <div className="flex-grow pb-40 bg-blue-100 shadow-inner">
         <div className="bg-blue-100 shadow-inner">
           <div className="w-full max-w-6xl m-auto">
-            <div className="px-4 py-2 space-y-4">
-              <Pagination
-                offset={offset}
-                setPage={setPage}
-                page={page}
-                pages={8}
-                resultsCount={totalCount}
-              />
-              {searchKeyword && (
-                <div>
-                  Search Results for:{' '}
-                  <h3 className="inline font-bold capitalize whitespace-nowrap text-md">
-                    {searchKeyword}
-                  </h3>
-                </div>
-              )}
-            </div>
+            {pageConfig && (
+              <div className="px-4 py-2 space-y-4">
+                <Pagination {...pageConfig} />
+                {searchKeyword && (
+                  <div>
+                    Search Results for:{' '}
+                    <h3 className="inline font-bold capitalize whitespace-nowrap text-md">
+                      {searchKeyword}
+                    </h3>
+                  </div>
+                )}
+              </div>
+            )}
             <div className="grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
               {loading
                 ? new Array(8)
@@ -94,12 +124,12 @@ const HomeScreen = () => {
                         key={idx}
                       />
                     ))
-                : animes &&
-                  animes.map((anime) => (
+                : displayedAnimes &&
+                  displayedAnimes.map((anime) => (
                     <AnimeCard anime={anime} key={anime.id} />
                   ))}
             </div>
-            {!loading && animes && animes.length <= 0 && (
+            {!loading && displayedAnimes && displayedAnimes.length <= 0 && (
               <div className="py-10 space-y-4">
                 <h2 className="text-3xl font-bold text-center">
                   No Results Found
